ea613964d835fe0def313e871b641154f06d5d97
ant-git <antonkhvdv@gmail.com>
2017-12-08 08:27:27 +0300

    Add WTForm and base template

---
+++ author/form.py
@@ -0,0 +1,17 @@
+from flask_wtf import Form
+from wtforms import validators, StringField, PasswordField
+from wtforms.fields.html5 import EmailField
+
+class RegisterForm(Form):
+    fullname = StringField('Full name', [validators.DataRequired()])
+    email = EmailField('Email', [validators.DataRequired()])
+    username = StringField('Username', [
+        validators.DataRequired(),
+        validators.Length(min=4, max=25)
+    ])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm',message='Password does not match'),
+        validators.Length(min=4, max=80)
+    ])
+    confirm = PasswordField('Repeat Password')
--- author/views.py
+++ author/views.py
@@ -1,5 +1,13 @@
+from flask import render_template, redirect
 from __init__ import app
+from author.form import RegisterForm
+
 
 @app.route('/login')
 def login():
     return "Hello user!"
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = RegisterForm()
+    return render_template('author/register.html', form=form)
