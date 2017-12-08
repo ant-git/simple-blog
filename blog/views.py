cf0d45ae28b3459a1b5f69b3c0c8fdc9241496df
ant-git <antonkhvdv@gmail.com>
2017-12-08 10:32:32 +0300

    Add setup view for blog

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,38 @@ def admin():
     return render_template('blog/admin.html')
 
 
-@app.route('/setup')
+@app.route('/setup', methods=('GET', 'POST'))
 def setup():
     form = SetupForm()
+    error = ""
+    # taking data from form and adding that data it to the table
+    if form.validate_on_submit():
+        author = Author(
+            form.fullname.data,
+            form.email.data,
+            form.username.data,
+            form.password.data,
+            True
+        )
+        db.session.add(author)
+        db.session()
+        db.session.flush()
+        if author.id:
+            blog = Blog(
+                form.name.data,
+                author.id
+            )
+            db.session.add(blog)
+            db.session.flush()
+        else:
+            db.session.rollback()
+            error = "Error Creating user"
+        if author.id and blog.id:
+            db.session.commit()
+            flash("Blog Created")
+            return redirect(url_for('admin'))
+        else:
+            db.session.rollback()
+            error = "Error Creating Blog"
+
     return render_template('blog/setup.html', form=form)
